e2ae3b2371f70febc9838ae37c024fccae5a0300
xyld <lamo0070@umn.edu>
2011-02-06 14:10:26 -0600

    forms: fixing imports
    Not sure how this code ever worked (maybe against a previous version of
    django?).

--- captcha/forms.py
+++ captcha/forms.py
@@ -3,7 +3,8 @@ from captcha.fields import ReCaptchaField
 from django.conf import settings
 from django.utils.encoding import smart_unicode, force_unicode
 from django.utils.translation import ugettext_lazy as _
-from django.forms import BoundField
+from django import forms
+from django.forms.forms import BoundField
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
 
